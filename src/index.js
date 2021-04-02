6f26feb5cd0c28d87a1a0ba19aacd27c0adf140d
thedon122 <donovan122@yahoo.com>
2021-04-01 23:48:58 -0400

    layed out index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,22 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const { verify } = require('jsonwebtoken');
 const { hash, compare } = require('bcryptjs');
+
+
+// 1. Register a user
+// 2. Login a user
+// 3. Logout a user
+// 4. Setup a protected route
+// 5. Get a new accesstoken with a refresh token
+
+const server = express();
+
+// Use express middleware for easier cookie handling
+server.use(cookieParser());
+
+server.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }),
+);
