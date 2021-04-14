f4c6ac130a8b77205afe0e8e9948885841e7764e
thedon122 <donovan122@yahoo.com>
2021-04-13 23:58:24 -0400

    update login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,43 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { navigate } from '@reach/router';
+import { UserContext } from '../App';
 
-const Login = () => <div>Login</div>
+const Login = () => {
+    const [user, setUser] = useContext(UserContext);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async e => {
+
+    }
+
+    const handleChange = e => {
+
+    }
+
+    return (
+        <div className="login-wrapper">
+            <form onSubmit={handleSubmit}>
+                <h2>Login</h2>
+                <input
+                value={email}
+                onChange={handleChange}
+                type="text"
+                name="email"
+                placeholder="Email"
+                autoComplete="email"
+                />
+                <input
+                value={password}
+                onChange={handleChange}
+                type="text"
+                name="password"
+                placeholder="Password"
+                autoComplete="password"
+                />
+            </form>
+        </div>
+    )
+}
 
 export default Login;
